7e486334cd387432239ab05caa58cb2760321359
Luís Miguel <148079567+LuisMIguelFurlanettoSousa@users.noreply.github.com>
2024-12-05 22:25:41 -0300

    ex
    05/12/2024

--- Seçao-3/aula32.py
+++ Seçao-3/aula32.py
@@ -3,7 +3,7 @@ Faça um programa que peça ao usuário para digitar um número inteiro,
 informe se este número é par ou ímpar. Caso o usuário não digite um número
 inteiro, informe que não é um número inteiro.
 """
-#Solução
+#Solução:
 # num = input("Digite um numero inteiro: ")
 
 # try:
@@ -22,6 +22,7 @@ Faça um programa que pergunte a hora ao usuário e, baseando-se no horário
 descrito, exiba a saudação apropriada. Ex. 
 Bom dia 0-11, Boa tarde 12-17 e Boa noite 18-23.
 """
+#Solução:
 
 # hr = input("Informe a hr certa: ")
 
@@ -44,17 +45,18 @@ Faça um programa que peça o primeiro nome do usuário. Se o nome tiver 4 letra
 menos escreva "Seu nome é curto"; se tiver entre 5 e 6 letras, escreva 
 "Seu nome é normal"; maior que 6 escreva "Seu nome é muito grande". 
 """
+#Solução:
 
-nome = input("Insira o seu primeiro nome: ")
+# nome = input("Insira o seu primeiro nome: ")
 
-if " " not in nome:
-    tamanho_nome = len(nome)
-    tamanho5_6 = tamanho_nome == 5 or tamanho_nome == 6
-    if tamanho_nome <= 4:
-        print("Seu nome é curto")
-    elif tamanho5_6:
-        print("Seu nome é normal")
-    else:
-        print("Seu nome é muito grande")
-else:
-    print("Insira apenas o primeiro nome!")
+# if " " not in nome:
+#     tamanho_nome = len(nome)
+#     tamanho5_6 = tamanho_nome == 5 or tamanho_nome == 6
+#     if tamanho_nome <= 4:
+#         print("Seu nome é curto")
+#     elif tamanho5_6:
+#         print("Seu nome é normal")
+#     else:
+#         print("Seu nome é muito grande")
+# else:
+#     print("Insira apenas o primeiro nome!")
